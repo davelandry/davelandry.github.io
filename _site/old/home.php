57a4af03d2b41c31b2538bd3217e16555e66bab7
Dave Landry <landry.dave@gmail.com>
2014-07-03 10:06:39 -0400

    design updates and re-org

--- _site/old/home.php
+++
@@ -1,4 +0,0 @@
-<h1>Dave</h1>
-There's not much to see here. I'm busy right now working at Growth Ventures. If you want, you can check out my <div class="link">Resume</div>, or contact me using the links below.
-
-<?php include('social.php'); ?>
